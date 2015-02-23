a6e3dad232a83ac98f19c5a5b716ea3326cbd5d6
Tristan Straub <tristanstraub@gmail.com>
2015-02-23 21:07:53 +1100

    Missing dep

--- src/cljs/threed/world.cljs
+++ src/cljs/threed/world.cljs
@@ -1,4 +1,5 @@
-(ns threed.world)
+(ns threed.world
+  (:require [threed.math :refer [vec-add vec-scale axis-rotation-matrix3 matrix-vec-mult]]))
 
 (defn generate-world [pos]
   (reduce concat (->> (range 10)
